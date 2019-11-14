bc075b93b034eb0d10f1f306af5ed74c41014a6c
Dimos Galatakis <galatakisd@cardiff.ac.uk>
2019-11-14 13:24:32 +0000

    Fixed some errors on createTeams.sql

--- createTeams.sql
+++ createTeams.sql
@@ -3,5 +3,8 @@ TeamId int primary key auto_increment,
 TeamName nvarchar(15));
 
 create table PTeams(
-foreign key (TeamID, PersonID) references Teams(TeamID), People(PeopleId)
+TeamID int not null,
+PersonID int not null,
+foreign key (TeamID) references Teams(TeamID)
+foreign key (PersonID) references People(PeopleId)
 constraint PTID primary key (TeamID, PersonID));
