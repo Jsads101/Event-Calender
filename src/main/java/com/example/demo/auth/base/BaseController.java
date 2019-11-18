ae862bea75c4934f5457348c3f218916ecf50ce0
Krzysztof Rachwal <RachwalK@cardiff.ac.uk>
2019-11-18 11:35:10 +0000

    Login interface files

---
+++ src/main/java/com/example/demo/auth/base/BaseController.java
@@ -0,0 +1,40 @@
+package com.example.demo.auth.base;
+
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class BaseController {
+
+    @GetMapping("/")
+    public ModelAndView home(HttpSession session) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("homeTemplate");
+        return mv;
+    }
+
+    @GetMapping("/userArea")
+    public ModelAndView userArea(HttpSession session) {
+        //For debugging, loop through the session attributes and print them
+        /*Enumeration<String> attributeNames = session.getAttributeNames();
+        while (attributeNames.hasMoreElements()){
+            System.out.println(attributeNames.nextElement());
+        }*/
+
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("userAreaTemplate");
+        return mv;
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        new SecurityContextLogoutHandler().logout(request, null, null);
+        return "redirect:/";
+    }
+
+}
