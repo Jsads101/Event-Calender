32b51b5138ddb0f9824561170e82e03237c6e853
Owen Harvey <harveyo@cardiff.ac.uk>
2019-12-02 16:31:12 +0000

    Delete Functionality

---
+++ src/main/java/com/example/demo/Controllers/CancelAttendingController.java
@@ -0,0 +1,29 @@
+package com.example.demo.Controllers;
+
+import com.example.demo.Data.CancelAttendingRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class CancelAttendingController {
+
+    private CancelAttendingRepo repo;
+
+    @Autowired
+    public CancelAttendingController(CancelAttendingRepo repo){
+        this.repo = repo;
+    }
+
+
+    @PostMapping("/cancelAttending")
+
+    public String cancelAttending(@RequestParam int peopleId, @RequestParam int eventId) {
+
+        repo.cancelAttending(peopleId, eventId);
+        return "redirect:/viewEvents";
+    }
+
+}
---
+++ src/main/java/com/example/demo/Data/CancelAttendingRepo.java
@@ -0,0 +1,38 @@
+package com.example.demo.Data;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.servlet.http.HttpSession;
+
+
+@Repository
+public class CancelAttendingRepo implements CancelAttendingInterface {
+
+    //Creating JDBC connection and giving it to class. Used to get data from database.
+//    @Autowired
+    private final JdbcTemplate jdbcTemplate;
+    private HttpSession session;
+
+    public CancelAttendingRepo(JdbcTemplate aTemplate, HttpSession session) {
+        jdbcTemplate = aTemplate;
+        this.session = session;
+    }
+
+    @Override
+    public int cancelAttending(int peopleId, int eventId) {
+        jdbcTemplate.update("DELETE FROM BookingStatus WHERE peopleId=(?) AND eventId=(?);",
+                getSessionID(), eventId
+        );
+        return 0;
+    }
+
+    public int getSessionID() {
+        return jdbcTemplate.queryForObject("select PeopleId from People where Email = ?",
+                new Object[]{session.getAttribute("SESSION_USERNAME")},
+                (rs, i) -> new Integer(
+                        rs.getInt("PeopleId")
+                )
+        );
+    }
+}
