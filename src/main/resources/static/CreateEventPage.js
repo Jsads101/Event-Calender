39b01a192ea393c037442586c1785038f49f5100
Jenny Sadler <->
2019-12-06 13:56:03 +0000

    Layout fixed in Create Event Page. Bugs in Javascript fixed

--- src/main/resources/static/CreateEventPage.js
+++ src/main/resources/static/CreateEventPage.js
@@ -25,8 +25,8 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn1.onclick = function() {
-    modal1.style.display = "block";
+btn.onclick = function() {
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -63,7 +63,7 @@ span2.onclick = function() {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == modal2) {
         modal2.style.display = "none";
     }
 }
