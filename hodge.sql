a51048be386861a56c78e60e7e59bdd97f437174
Jennifer Sadler <sadlerjm@cardiff.ac.uk>
2020-02-24 11:25:25 +0000

    Update hodge.sql

--- hodge.sql
+++ hodge.sql
@@ -1,23 +1,20 @@
-CREATE DATABASE `c1973857_Team5` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE `c1981660_TestDB` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE 'c1981660_TestDB'
+CREATE TABLE `People` (
+  `PeopleId` int(11) NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `Surname` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `Email` varchar(40) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `Enabled` tinyint(4) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`PeopleId`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 CREATE TABLE `authorities` (
   `PeopleID` int(11) NOT NULL,
   `authority` varchar(50) NOT NULL,
   PRIMARY KEY (`PeopleID`,`authority`),
   CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`PeopleID`) REFERENCES `People` (`PeopleId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-CREATE TABLE `BookingStatus` (
-  `bookingStatusId` int(11) NOT NULL AUTO_INCREMENT,
-  `peopleId` int(11) NOT NULL,
-  `eventId` int(11) NOT NULL,
-  `statusId` int(11) NOT NULL,
-  `dietReq` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `teammembers` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`bookingStatusId`),
-  KEY `BookingStatus_ibfk_2` (`eventId`),
-  KEY `BookingStatus_ibfk_3` (`statusId`),
-  CONSTRAINT `BookingStatus_ibfk_2` FOREIGN KEY (`eventId`) REFERENCES `Events` (`EventId`),
-  CONSTRAINT `BookingStatus_ibfk_3` FOREIGN KEY (`statusId`) REFERENCES `Status` (`StatusId`)
-) ENGINE=InnoDB AUTO_INCREMENT=193 DEFAULT CHARSET=latin1;
 CREATE TABLE `Events` (
   `EventId` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(100) CHARACTER SET utf8 NOT NULL,
@@ -31,22 +28,21 @@ CREATE TABLE `Events` (
   `TeamSize` int(11) DEFAULT NULL,
   PRIMARY KEY (`EventId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=184 DEFAULT CHARSET=latin1;
-CREATE TABLE `People` (
-  `PeopleId` int(11) NOT NULL AUTO_INCREMENT,
-  `FirstName` varchar(20) CHARACTER SET utf8 NOT NULL,
-  `Surname` varchar(20) CHARACTER SET utf8 NOT NULL,
-  `Email` varchar(40) CHARACTER SET utf8 NOT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `Enabled` tinyint(4) NOT NULL DEFAULT 1,
-  PRIMARY KEY (`PeopleId`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
-CREATE TABLE `PTeams` (
-  `TeamID` int(11) NOT NULL,
-  `PeopleID` int(11) NOT NULL,
-  PRIMARY KEY (`TeamID`,`PeopleID`),
-  KEY `PeopleId` (`PeopleID`),
-  CONSTRAINT `PTeams_ibfk_1` FOREIGN KEY (`TeamID`) REFERENCES `Teams` (`TeamId`),
-  CONSTRAINT `PeopleId` FOREIGN KEY (`PeopleID`) REFERENCES `People` (`PeopleId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+CREATE TABLE `Status` (
+  `StatusId` int(11) NOT NULL AUTO_INCREMENT,
+  `Stat` varchar(10) NOT NULL,
+  PRIMARY KEY (`StatusId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+CREATE TABLE `Teams` (
+  `TeamId` int(11) NOT NULL AUTO_INCREMENT,
+  `TeamName` varchar(15) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`TeamId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `enabled` tinyint(4) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 CREATE TABLE `SPRING_SESSION` (
   `PRIMARY_ID` char(36) NOT NULL,
@@ -68,19 +64,17 @@ CREATE TABLE `SPRING_SESSION_ATTRIBUTES` (
   PRIMARY KEY (`SESSION_PRIMARY_ID`,`ATTRIBUTE_NAME`),
   CONSTRAINT `SPRING_SESSION_ATTRIBUTES_FK` FOREIGN KEY (`SESSION_PRIMARY_ID`) REFERENCES `SPRING_SESSION` (`PRIMARY_ID`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
-CREATE TABLE `Status` (
-  `StatusId` int(11) NOT NULL AUTO_INCREMENT,
-  `Stat` varchar(10) NOT NULL,
-  PRIMARY KEY (`StatusId`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-CREATE TABLE `Teams` (
-  `TeamId` int(11) NOT NULL AUTO_INCREMENT,
-  `TeamName` varchar(15) CHARACTER SET utf8 DEFAULT NULL,
-  PRIMARY KEY (`TeamId`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-CREATE TABLE `users` (
-  `username` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `enabled` tinyint(4) NOT NULL DEFAULT 1,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `BookingStatus` (
+  `bookingStatusId` int(11) NOT NULL AUTO_INCREMENT,
+  `peopleId` int(11) NOT NULL,
+  `eventId` int(11) NOT NULL,
+  `statusId` int(11) NOT NULL,
+  `dietReq` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `teammembers` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`bookingStatusId`),
+  KEY `BookingStatus_ibfk_2` (`eventId`),
+  KEY `BookingStatus_ibfk_3` (`statusId`),
+  CONSTRAINT `BookingStatus_ibfk_2` FOREIGN KEY (`eventId`) REFERENCES `Events` (`EventId`),
+  CONSTRAINT `BookingStatus_ibfk_3` FOREIGN KEY (`statusId`) REFERENCES `Status` (`StatusId`)
+) ENGINE=InnoDB AUTO_INCREMENT=193 DEFAULT CHARSET=latin1;
