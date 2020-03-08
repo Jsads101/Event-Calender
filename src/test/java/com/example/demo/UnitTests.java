bb8f69ff14cabcc67afa7fdc31c4c7fa684ee6ab
Jenny Sadler <->
2020-03-08 16:28:48 +0000

    Adding test resource folders to inplement test database

--- src/test/java/com/example/demo/UnitTests.java
+++ src/test/java/com/example/demo/UnitTests.java
@@ -1,4 +1,4 @@
-package com.example.demo.MainTests;
+package com.example.demo;
 
 import com.example.demo.Data.BookingStatus;
 import com.example.demo.Data.CreateEvent;
---
+++ src/test/resources/data.sql
@@ -0,0 +1,1 @@
+insert into bookingstatus values(1, 1, 1, 1, 'none', 2);
