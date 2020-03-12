9573e1753a44a244a2b16254c5cd64625a6f7e48
Jenny Sadler <->
2020-03-12 12:25:20 +0000

    Workaround implemented for authorisation issue. Tests for GetMapping Controllers will compile and pass, but PostMapping Controller test compiles but fails due to status 403 forbidden error. Still need to create new database in schema.sql every time i run a test to get it to compile

--- src/test/java/com/example/demo/Tests/MockMVCs.java
+++ src/test/java/com/example/demo/Tests/MockMVCs.java
@@ -1,6 +1,7 @@
 package com.example.demo.Tests;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -8,8 +9,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.http.HttpSession;
 
@@ -31,16 +34,16 @@ public class MockMVCs {
     @org.junit.Test
     public void testCancelAttendingController() throws Exception {
 
-        session.setAttribute("SESSION_USERNAME", "SadlerJM@cardiff.ac.uk");
-
-        this.mockMvc.perform(get("/cancelAttending?eventId=1")).andDo(print()).andExpect(status().isOk()).andExpect(redirectedUrl("/viewEvents"));
+        this.mockMvc.perform(post("/cancelAttending?eventId=1")).andDo(print()).andExpect(status().isOk()).andExpect(redirectedUrl("/viewEvents"));
     }
 
     @org.junit.Test
-    public void a_testGreeting() throws Exception {
-        this.mockMvc.perform(get("/greeting")).andDo(print())
-                .andExpect(content().string(containsString("Guest")));
+    public void testRegController() throws Exception {
+
+        this.mockMvc.perform(get("/register")).andDo(print()).andExpect(status().isOk()).andExpect(view().name("registerTemplate"));
     }
+
+
 
 
 }
